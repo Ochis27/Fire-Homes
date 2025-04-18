146f9d8b132cd057badb5135332f917a537ee67f
Ochis27 <ochispaulcatalin27@gmail.com>
2025-04-18 18:53:50 +0300

    first commit

---
+++ firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6rm6zMM8Zrs5ZSX65dRtpBiLic8XqfYw",
+  authDomain: "fire-homes-5be46.firebaseapp.com",
+  projectId: "fire-homes-5be46",
+  storageBucket: "fire-homes-5be46.firebasestorage.app",
+  messagingSenderId: "651884796057",
+  appId: "1:651884796057:web:c906940d8b746e72ef4008",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
